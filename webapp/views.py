bdd8bec8d929087ea69aa786b0eebc9eb865bd6f
Shub Raj Lama <shuvraj1234@gmail.com>
2022-02-17 11:38:53 +0545

    userpasstestmxn added

--- webapp/views.py
+++ webapp/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.mixins import UserPassesTestMixin,LoginRequiredMixin
 class Login(UserPassesTestMixin,LoginView):
     template_name = "webapp/login.html"
     def test_func(self):
-        if self.request.user.is_authenticated:
-            return redirect("app_webapp:home")
+        return not self.request.user.is_authenticated
+    def handle_no_permission(self):
+        return redirect("app_webapp:home")
 class Home(LoginRequiredMixin,TemplateView):
     template_name = "webapp/home.html"
